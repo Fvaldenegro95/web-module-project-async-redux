74707c453170793cb1045fe2182f764aa89f0b99
Fvaldenegro95 <fvaldenegro95@gmail.com>
2021-11-03 19:58:42 -0400

    mvp

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Cat from './components/Cat'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Cat App</h1>
+      </header>
+      <Cat/>
+    </div>
+  );
+}
+
+export default App;
